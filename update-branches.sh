7c9079ee55e019e875d196f2d231a03476f4e369
Tim <tim.tannert@tu-dortmund.de>
2019-10-21 21:24:20 +0200

    Update Skripte hinzugefügt

--- update-branches.sh
+++ update-branches.sh
@@ -1,1 +1,24 @@
-#!/bin/bash/
+#!/bin/bash
+git checkout master &&
+git pull &&
+git checkout space_efficient &&
+git merge --no-edit master &&
+git push &&
+git checkout fnv_hash &&
+git merge --no-edit master &&
+git push &&
+git checkout fnv_build &&
+git merge --no-edit master &&
+git push &&
+git checkout rustc_hash_hash &&
+git merge --no-edit master &&
+git push &&
+git checkout rustc_hash_build &&
+git merge --no-edit master &&
+git push &&
+git checkout hash_brown_hash &&
+git merge --no-edit master &&
+git push &&
+git checkout hash_brown_build &&
+git merge --no-edit master &&
+git push 
