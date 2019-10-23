05f7493beb959466ff01c6d442ed6f0e7651b167
Tim <tim.tannert@tu-dortmund.de>
2019-10-23 17:45:50 +0200

    test rb baum

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::fs::read_dir;
 pub const SAMPLE_SIZE: usize = 100;
 pub const REPEATS: usize = 100_000;
 
-
+use treez::rb::TreeRb;
 fn main() {	
     for dir in read_dir(format!("testdata/normal/u40/")).unwrap() {
         let path = dir.unwrap().path();
@@ -22,19 +22,23 @@ fn main() {
         let test_values = get_test_values(values[0]+1u32,values[values_len-1]);
 
         let bs = BinarySearch::new(values.clone());
-        let stree = STree::new(values.clone());
-        
+
+        let mut rb = TreeRb::<uint::u40,uint::u40>::new();
+
+        for i in values.into_iter() {
+            rb.insert(*i, *i);
+        }
         for (i,val) in test_values.into_iter().enumerate() {
             if i % 1000 == 0{
                 println!("{}", val);
             }
-            if bs.predecessor(val) != stree.predecessor(val) {
-                panic!("Gesucht: {} , bs_found {:?}, stree_found {:?}, data={:?}", val, bs.predecessor(val), stree.predecessor(val), path);
+            if bs.predecessor(val) != rb.predecessor(val).map(|x| *x) {
+                panic!("Gesucht: {} , bs_found {:?}, stree_found {:?}, data={:?}", val, bs.predecessor(val), rb.predecessor(val).map(|x| *x), path);
             }
 
-            if bs.successor(val) != stree.successor(val) {
-                panic!("Gesucht: {} , bs_found {:?}, stree_found {:?}, data={:?}", val, bs.successor(val), stree.successor(val), path);
-            }
+           /* if bs.successor(val) != rb.successor(val) {
+                panic!("Gesucht: {} , bs_found {:?}, stree_found {:?}, data={:?}", val, bs.successor(val), rb.successor(val), path);
+            }*/
 
             
         }
