ed735498aee223a8c70f2d44ecafd83fcd0288f3
Tim <tim.tannert@tu-dortmund.de>
2019-10-23 14:23:26 +0200

    Fixed skript

--- update-branches.sh
+++ update-branches.sh
@@ -2,29 +2,38 @@
 git checkout master &&
 git pull &&
 git checkout space_efficient &&
+git pull &&
 git merge --no-edit master &&
 git push &&
 git checkout fnv_hash &&
+git pull &&
 git merge --no-edit master &&
 git push &&
 git checkout fnv_build &&
+git pull &&
 git merge --no-edit master &&
 git push &&
 git checkout rustc_hash_hash &&
+git pull &&
 git merge --no-edit master &&
 git push &&
 git checkout rustc_hash_build &&
+git pull &&
 git merge --no-edit master &&
 git push &&
 git checkout hash_brown_hash &&
+git pull &&
 git merge --no-edit master &&
 git push &&
 git checkout hash_brown_build &&
+git pull &&
 git merge --no-edit master &&
 git push 
 git checkout space_efficient_128 &&
+git pull &&
 git merge --no-edit master &&
 git push 
 git checkout space_efficient_max &&
+git pull &&
 git merge --no-edit master &&
 git push 
